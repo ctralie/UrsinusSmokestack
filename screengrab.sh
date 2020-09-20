f613538da377bc59b85dd768e9b8eee9187a7cdc
Chris Tralie <chris.tralie@gmail.com>
2020-09-20 11:09:43 -0400

    Minor tweak

--- screengrab.sh
+++ screengrab.sh
@@ -6,11 +6,14 @@ sleep 5
 Firefox_PID=$!
 while [ 1 ]
 do
-    sleep 3
+    sleep 4
     DISPLAY=:3 xdotool mousemove 300 300 click 1
+    sleep 1
     filename=A_$(date '+%Y-%m-%d-%k-%M-%S')".png"
     import -display :3 -window root $filename
+    sleep 4
     DISPLAY=:3 xdotool mousemove 300 300 click 1
+    sleep 1
     filename=B_$(date '+%Y-%m-%d-%k-%M-%S')".png"
     import -display :3 -window root $filename
 done
