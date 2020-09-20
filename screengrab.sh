9225d4f25092e8529a9e5e40afeaea0862d0eb3f
Chris Tralie <chris.tralie@gmail.com>
2020-09-20 10:38:55 -0400

    Adding basic bash hack for doing a time lapse

---
+++ screengrab.sh
@@ -0,0 +1,18 @@
+Xvfb :3 -screen 0 1024x768x24 &
+FrameBuf_PID=$!
+echo FrameBuffer PID: $FrameBuf_PID
+DISPLAY=:3 firefox https://panda.ursinus.edu/smokestack/ &
+sleep 5
+Firefox_PID=$!
+while [ 1 ]
+do
+    sleep 3
+    DISPLAY=:3 xdotool mousemove 300 300 click 1
+    filename=A_$(date '+%Y-%m-%d-%k-%M-%S')".png"
+    import -display :3 -window root $filename
+    DISPLAY=:3 xdotool mousemove 300 300 click 1
+    filename=B_$(date '+%Y-%m-%d-%k-%M-%S')".png"
+    import -display :3 -window root $filename
+done
+kill $Firefox_PID
+kill $FrameBuf_PID
